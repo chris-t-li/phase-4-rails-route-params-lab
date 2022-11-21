4412957515516cc87cad23105c6bd9c8acf7aac4
Chris Li <chris.lty07@gmail.com>
2022-11-21 11:58:38 -0500

    commit tree d98d6f3cc07335a08976b0accd28b300420bfbcb

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,21 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    if params[:name]
+      search_name = params[:name].capitalize
+      students = Student.where(first_name: search_name).or(Student.where(last_name: search_name)) 
+    else
+      students = Student.all
+    end
+    render json: students 
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+  def search
+    student = Student.find
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   get '/students', to: 'students#index'
   get '/students/grades', to: 'students#grades'
   get '/students/highest-grade', to: 'students#highest_grade'
+  get '/students/:id', to: 'students#show'
+  get '/students?name=:name', to: 'students#index'
+  # get '/students?first_name=:first_name&last_name=:last_name', to: 'students.search'
 end
